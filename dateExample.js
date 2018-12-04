b6a9e6705e4c711782abd3147def337040ba0bc4
sanamestri <45585391+sanamestri@users.noreply.github.com>
2018-12-04 13:41:38 +0530

    Update dateExample.js

--- dateExample.js
+++ dateExample.js
@@ -1,3 +1,4 @@
+/* JavaScript program to find 1st January is being a Sunday between 2014 and 2050 */
 var checkDate = new Date(2014, 0, 1);
 for(i=0; i< 50 ; i++) {if(checkDate.getFullYear()== 2050) {break;}
  if(checkDate.getDay() == 0){
